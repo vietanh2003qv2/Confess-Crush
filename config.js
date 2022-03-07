840fb02bbb8c0a6937bca38372522b0c3f4d34f9
vietanh2003qv2 <84821574+vietanh2003qv2@users.noreply.github.com>
2022-03-08 02:21:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "I LOVE YOU BBI",
+    introTitle: 'Nguyen Thi Duyen',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
